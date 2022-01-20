8612642b0b360c3748d5b5e672240e340e0c53d5
Paul Cowan <paul.cowan@cutting.scot>
2022-01-20 18:57:52 +0000

    renam .eslintrc

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@cutting/eslint-config/node'],
-  rules: {
-    'eslint-disable jest/consistent-test-it': ['off'],
-    'no-done-callback': ['off'],
-  },
-};
